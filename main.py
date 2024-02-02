449f4b95ff73be32ca8bf567167aea377405dae2
hhjhkkljj <inakinnikita605@gmail.com>
2024-02-02 15:11:18 +0300

    7

---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+st.set_page_config(page_title="Amkrlv")
+st.title("Графопостроитель")
+st.subheader("Отправьте мне файл excel")

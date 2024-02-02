618ab14b3be82ef91d51d4cd179d43a567d1b144
hhjhkkljj <inakinnikita605@gmail.com>
2024-02-02 15:18:21 +0300

    9

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+st.set_page_config(page_title="megatronus")
+st.title("Графопостроитель")
+st.subheader("Отправьте мне файл excel")
+
+uploaded_file = st.file_uploader("Выберите файл", type="xlsx")
+if uploaded_file:
+    df = pd.read_excel(uploaded_file, engine="openpyxl")
+    st.dataframe(df)
+    groupby_column = st.selectbox(
+        "Что вы хотите проанализировать?",
+        ("Ship Mode", "Segment", "Category", "Sub-Category"),
+    )
+    output_columns = ["Sales", "Profit"]
+    df_grouped = df.groupby(by=[groupby_column], as_index=False)[output_columns].sum()
+    st.dataframe(df_grouped)
